b22fc9c52d768ecfe5fa268a6bffede33fd42269
ara-umi <532990165@qq.com>
2022-06-12 06:11:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,8 @@
 # Auth0r: ara_umi
 # Email: 532990165@qq.com
 # DateTime: 2022/6/7 0:05
-from client_http import TickerPrice, per_5minutes, Timer, per_minute
+
+from client_ws import main
 
 if __name__ == '__main__':
-    ticker = TickerPrice()
-
-    def main():
-        try:
-            data = ticker.query()
-            ticker.dump2mysql(data)
-        except Exception as e:
-            print(e)
-            ticker.dumper.db.close()
-
-    timer = Timer(main, per_minute)
-    timer.run()
+   main()
